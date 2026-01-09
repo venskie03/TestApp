7e7c08e91d3d0c25a8b45be119f37c7c1d12a753
venskie03 <kevinkyleganados03@gmail.com>
2026-01-10 04:32:33 +0800

    adding serverless for vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
-import http from "http";
+import serverless from "serverless-http";
 import geminiRoutes from "./routes/gemini/gemini.ts";
 
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
 const API_PATH = "/api/v1";
-
 app.use(`${API_PATH}/gemini`, geminiRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`);
-});
+export const handler = serverless(app);
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Local server running at http://localhost:${PORT}`);
+  });
+}
