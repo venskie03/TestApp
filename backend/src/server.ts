4e5d348af702c5b57115d5a73593f05afbcb182a
venskie03 <kevinkyleganados03@gmail.com>
2026-01-10 04:44:12 +0800

    adding vercel json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,22 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
-import serverless from "serverless-http";
+import http from "http";
 import geminiRoutes from "./routes/gemini/gemini.ts";
 
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 const API_PATH = "/api/v1";
+
 app.use(`${API_PATH}/gemini`, geminiRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-export const handler = serverless(app);
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Local server running at http://localhost:${PORT}`);
-  });
-}
+server.listen(port, () => {
+  console.log(`🚀 Server running at ${port}`);
+});
